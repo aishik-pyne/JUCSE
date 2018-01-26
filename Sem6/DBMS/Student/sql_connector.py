ef6076f9c550ad78b3d061d6b8e6097af5f0588d
Aishik Pyne <aishikpyne@gmail.com>
2018-01-27 00:32:14 +0530

    sql connector , addStudent.ui , addStudent.py

---
+++ Sem6/DBMS/Student/sql_connector.py
@@ -0,0 +1,30 @@
+import pymysql.cursors
+
+class SqlConnector:
+    def __init__(self):
+        self.connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='mummum10',
+                             db='university',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+    def get_dept_names(self):
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT * FROM `departments` "
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                return [r['dept_short_name'] for r in result]
+        except Exception as e:
+            print(e)
+
+    def add_student(self, student):
+        try:
+            with self.connection.cursor() as cursor:
+                sql = """INSERT INTO `students` (name, dept) VALUES ('{0}','{1}');""".format(student['name'], student['dept'])
+                cursor.execute(sql)
+            self.connection.commit()
+        except Exception as e:
+            print(e)
