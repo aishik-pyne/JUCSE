edc07b2be0abfc55b6768b4655d4327642027ea2
Your Name <aishikpyne@gmail.com>
2018-03-15 16:43:12 +0530

    Ass3

---
+++ Sem6/DBMS/Ass3/dump.sql
@@ -0,0 +1,92 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: 35.226.166.22    Database: organization
+-- ------------------------------------------------------
+-- Server version	5.7.14-google-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED='ba90ff5b-0e6b-11e8-862e-42010a800fdb:1-2820230';
+
+--
+-- Table structure for table `DEPARTMENT`
+--
+
+DROP TABLE IF EXISTS `DEPARTMENT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DEPARTMENT` (
+  `DEPT_CODE` char(5) NOT NULL,
+  `DEPT_NAME` char(15) DEFAULT NULL,
+  PRIMARY KEY (`DEPT_CODE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `EMP`
+--
+
+DROP TABLE IF EXISTS `EMP`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EMP` (
+  `EMP_CODE` varchar(5) NOT NULL,
+  `EMP_NAME` varchar(20) DEFAULT NULL,
+  `DEPT_CODE` varchar(5) DEFAULT NULL,
+  `DESIG_CODE` varchar(5) DEFAULT NULL,
+  `SEX` varchar(1) DEFAULT NULL,
+  `ADDRESS` varchar(25) DEFAULT NULL,
+  `CITY` varchar(20) DEFAULT NULL,
+  `STATE` varchar(20) DEFAULT NULL,
+  `PIN` varchar(6) DEFAULT NULL,
+  `BASIC` decimal(10,0) DEFAULT '0',
+  `JOIN_DATE` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`EMP_CODE`),
+  KEY `DEPT_CODE` (`DEPT_CODE`),
+  CONSTRAINT `EMP_ibfk_1` FOREIGN KEY (`DEPT_CODE`) REFERENCES `DEPARTMENT` (`DEPT_CODE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `LEAVE_REC`
+--
+
+DROP TABLE IF EXISTS `LEAVE_REC`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `LEAVE_REC` (
+  `EMP_CODE` varchar(5) NOT NULL,
+  `LEAVE_TYPE` enum('CL','EL','ML') DEFAULT NULL,
+  `FROM_DATE` datetime NOT NULL,
+  `TO_DATE` datetime DEFAULT NULL,
+  PRIMARY KEY (`EMP_CODE`,`FROM_DATE`),
+  CONSTRAINT `LEAVE_REC_ibfk_1` FOREIGN KEY (`EMP_CODE`) REFERENCES `EMP` (`EMP_CODE`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-15 15:33:54
