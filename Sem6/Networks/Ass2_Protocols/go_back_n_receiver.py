ad06117e20eb80b5f4ef7768f29f763629084908
Aishik Pyne <aishikpyne@gmail.com>
2018-03-21 23:21:04 +0530

    Ass3 Compiler Lab Update

---
+++ Sem6/Networks/Ass2_Protocols/go_back_n_receiver.py
@@ -0,0 +1,62 @@
+from socket import *
+from decimal import *
+import os
+from random import random
+import time
+import sys
+from crc import CRC
+from collections import namedtuple
+import pickle
+
+# Method to send to client
+def rdt_send(exp_seq_no):
+	return ack_pkt(exp_seq_no, True)
+
+# Write to File
+def f_write(decoded,f_name):
+    with open(f_name, 'a') as f:
+        f.write(decoded)
+
+if __name__ == '__main__':
+    # Collect CLI arguments
+    port = int(sys.argv[1])
+    output_file = sys.argv[2]
+    probability = float(sys.argv[3])
+    hostname = ""
+
+    # Setup CRC
+    crc = CRC()
+    data_pkt = namedtuple('data_pkt', 'seq_num frame')
+    ack_pkt = namedtuple('ack_pkt', 'seq_num ack')
+
+    # Communication
+    s = socket(AF_INET,SOCK_DGRAM)
+    s.bind((hostname,port))
+    print ("Listening for client requests ... ")
+    exp_seq_no = 0
+    while 1:
+    	msg, client_address = s.recvfrom(65535)
+        data = pickle.loads(msg)
+        got_seq_no, frame = data.seq_no, data.frame
+    	if random() > probability: # Random packet drop simulation
+    		decoded = crc.decode([frame])
+    		if decode not None: # Checksum is correct
+    			if got_seq_no == exp_seq_no: # Send ack
+    				to_send = rdt_send(exp_seq_no)
+    				if to_send:
+    					s.sendto(to_send, client_address)
+    					f_write(decoded,output_file)
+    					exp_seq_no = exp_seq_no + 1
+    			elif got_seq_no > exp_seq_no: # Future packet received, hence dropped
+    				print ("Packet loss, sequence number = " + str(got_seq_no))
+    			elif got_seq_no < exp_seq_no: # Repeat sent
+    				to_send = rdt_send(exp_seq_no)
+    				if to_send:
+    					# print ("Retransmitted ACK - " + str(got_seq_no))
+    					s.sendto(to_send, client_address)
+    		else:
+    			print ("Checksum invalid. Packet dropped.")
+    	else: # Random packet drop simulation
+    		print ("Packet loss, sequence number = " + str(got_seq_no))
+
+    s.close()
