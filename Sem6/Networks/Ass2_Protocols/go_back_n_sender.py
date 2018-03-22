229500deb1f883e05e6c9b7a6316b4237cb20bd3
aishik-pyne <aishikpyne@gmail.com>
2018-03-22 14:20:36 +0530

    Mar 22 Go back N

--- Sem6/Networks/Ass2_Protocols/go_back_n_sender.py
+++ Sem6/Networks/Ass2_Protocols/go_back_n_sender.py
@@ -8,6 +8,8 @@ from collections import namedtuple
 from crc import CRC
 from socket import *
 import pickle
+from time import sleep
+import threading
 # Set Sent sequence number
 # def seq_sent(seq_no,tempfile):
 # 	fptr = open(tempfile, "r+w")
@@ -25,56 +27,31 @@ import pickle
 # Process to receive
 def rcv_thread(soc):
 	global window_size
-	global window_low
-	global window_high
 	global total_frames
+	global tempfile
 	new_proc = os.fork()
 	if new_proc == 0:
 		print("Receive Thread " + str(os.getpid()) + " created..")
 		while 1:
+			while os.stat(tempfile).st_size == 0:
+				continue
+
+			status = pickle.load(open(tempfile, 'rb'))
+			print(status)
 			message, server_addr = soc.recvfrom(max_buff)
 			message = pickle.loads(message)
-			seq_no, ack = message['seq_no'], message['ack']
+			seq_no, ack = message['seq_num'], message['ack']
+			print('\033[93mAck Rec {} value {}\033[0m'.format(seq_no, ack))
 			if seq_no == total_frames-1:
-				transfer_complete = True
+				status['transfer_complete'] = True
 				break
 			if ack:
-				window_low = seq_no+1
-				window_high = window_low + window_size
-
-
-# # Timers
-# def set_timeout(send_seq_no,tempfile):
-# 	child_process = os.fork()
-# 	if child_process == 0:
-# 		p = psutil.Process(os.getpid())
-# 		time.sleep(0.5)
-# 		fptr = open(tempfile,'r+w')
-# 		fcntl.flock(fptr.fileno(),fcntl.LOCK_EX)
-# 		update_seq_status = -1
-# 		data = fptr.readlines()
-# 		for line in data:
-# 			match = re.findall('(\d+),(\w)\n',line)
-# 			if match:
-# 				if send_seq_no == str(match[0][0]):
-# 					if str(match[0][1]) == 'C':
-# 						break
-# 					elif str(match[0][1]) == 'W':
-# 						print( "Timeout, sequence number =:" + send_seq_no)
-# 						update_seq_status = 1
-# 						break
-# 					else:
-# 						break
-# 		if update_seq_status == 1:
-# 			if data[int(send_seq_no)] == send_seq_no + ",W\n":
-# 				data[int(send_seq_no)] = send_seq_no + ",\n"
-# 			new_data = ''.join(data)
-# 			fptr.seek(0)
-# 			fptr.truncate()
-# 			fptr.write(new_data)
-# 		fptr.close()
-# 		os._exit(0)
-#
+				status['window_low'] = seq_no+1
+				status['window_high'] = status['window_low'] + window_size
+				x = open(tempfile, 'wb')
+				fcntl.flock(x, fcntl.LOCK_EX )
+				pickle.dump(status, open(tempfile, 'wb') )
+				fcntl.flock(x, fcntl.LOCK_UN)
 
 def pack_data(seq_no, frame):
 	return pickle.dumps({'seq_num': seq_no, 'frame':frame})
@@ -88,7 +65,7 @@ if __name__ == '__main__':
 	port = int(sys.argv[1])
 	send_file = sys.argv[2]
 	window_size = 8
-
+	tempfile = 'tempfile.txt'
 	# Set the maximum buffer size
 	max_buff = 65535
 
@@ -104,10 +81,12 @@ if __name__ == '__main__':
 	total_frames = len(frames)
 	ack_recv_till = 0
 	seq_no_to_send = 0
-	transfer_complete = False
-	window_low = 0
-	window_high = window_size -1
-
+	status = {
+		'transfer_complete' : False,
+		'window_low' : 0,
+		'window_high' : window_size -1,
+	}
+	pickle.dump(status, open(tempfile, 'wb'))
 	# COMMS
 	client_socket = socket(AF_INET,SOCK_DGRAM)
 
@@ -116,17 +95,23 @@ if __name__ == '__main__':
 
 	# Send file
 	while 1:
-		if transfer_complete:
+		x = open(tempfile, 'rb')
+		fcntl.flock(x, fcntl.LOCK_EX )
+		status = pickle.load(x)
+		fcntl.flock(x, fcntl.LOCK_UN)
+		# print(status)
+		if status['transfer_complete']:
 			print( "File transfer Complete ..")
 			print( "Total time in seconds : " + str(time.time() - start_time))
 			break
 
+		print('Trying to send {}'.format(seq_no_to_send))
 		message_to_send = pack_data(seq_no_to_send, frames[seq_no_to_send])
 		client_socket.sendto(message_to_send,(server_host,port))
 		# set_timeout(send_seq_no,tempfile)
 		seq_no_to_send += 1
-		if seq_no_to_send > window_high:
-			seq_no_to_send = window_low
+		if seq_no_to_send > status['window_high']:
+			seq_no_to_send = status['window_low']
 
 	# Close socket
 	client_socket.close()
